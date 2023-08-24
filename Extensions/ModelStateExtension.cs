ebdb07e08347667e276c5b516929b4d110b74c99
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-08-23 23:05:06 -0300

    Extensões de Erros de um ModelState e transformando Role em claims

---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BlogAPI.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            return modelState.SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage)
+                .ToList();
+        }
+    }
+}
---
+++ Extensions/RoleClaimsExtension.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Models;
+using System.Security.Claims;
+
+namespace BlogAPI.Extensions
+{
+    public static class RoleClaimsExtension
+    {
+        public static IEnumerable<Claim> GetClaims(this User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+            claims.AddRange(user.Roles.Select(role => new Claim(ClaimTypes.Role, role.Slug)));
+            return claims;
+        }
+    }
+}
