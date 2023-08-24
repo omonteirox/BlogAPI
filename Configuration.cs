78bf7833b30570eae049d44733a0afbc0f6269fb
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-08-24 19:45:26 -0300

    Começo de configuração de smtp

--- Configuration.cs
+++ Configuration.cs
@@ -6,6 +6,16 @@
         public static string JwtKey = "I0p3beps/kijsPm3wiMQnw==";
 
         public static string ApiKeyName = "api_key";
+
         public static string ApiKey = "curso_api_kij3nwPmpw/MQ==iss";
+        public static SmtpConfiguration Smtp = new();
+
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
