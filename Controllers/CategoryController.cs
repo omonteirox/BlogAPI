93d015bea9b433f3499631a932f7a49edeed9d01
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-08-23 23:03:20 -0300

    Controller de categoria

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using Blog.Data;
+using Blog.Models;
+using BlogAPI.Extensions;
+using BlogAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")]
+        public async Task<IActionResult> GetAsync([FromServices] BlogDataContext ctx)
+        {
+            try
+            {
+                var categories = await ctx.Categories.AsNoTracking().ToListAsync();
+                return Ok(new ResultViewModel<List<Category>>(categories));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<List<Category>>("CAT-001 Falha interna no Servidor"));
+            }
+        }
+
+        [HttpGet("categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromServices] BlogDataContext ctx, [FromRoute] int id)
+        {
+            try
+            {
+                var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound(new ResultViewModel<Category>("CAT-003 Categoria Não encontrada."));
+
+                return Ok(new ResultViewModel<Category>(category));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+            }
+        }
+
+        [HttpDelete("categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync([FromServices] BlogDataContext ctx, [FromRoute] int id)
+        {
+            try
+            {
+                var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound(new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+                ctx.Categories.Remove(category);
+                await ctx.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-002 Não foi possível adicionar a categoria"));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+            }
+        }
+
+        [HttpPost("categories")]
+        public async Task<IActionResult> PostAsync([FromServices] BlogDataContext ctx, [FromBody] EditorCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+            try
+            {
+                var category = new Category
+                {
+                    Id = 0,
+                    Name = model.Name,
+                    Slug = model.Slug.ToLower()
+                };
+                await ctx.Categories.AddAsync(category);
+                await ctx.SaveChangesAsync();
+                return Created($"/v1/categories/{category.Id}", new ResultViewModel<Category>(category));
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-002 Não foi possível Remover a categoria"));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+            }
+        }
+
+        [HttpPut("categories/{id:int}")]
+        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext ctx, [FromRoute] int id, [FromBody] EditorCategoryViewModel model)
+        {
+            try
+            {
+                var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound(new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+                ctx.Update(category);
+                await ctx.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-002 Não foi possível Atualizar a categoria"));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("CAT-001 Falha interna no Servidor"));
+            }
+        }
+    }
+}
