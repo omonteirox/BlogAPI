4e99f533757ba4948ec54d48f155f8a9a284e75d
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-08-23 23:04:14 -0300

    Health check utilizando a ApiKey

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using BlogAPI.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+// healthCheck
+namespace BlogAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        [ApiKey]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
