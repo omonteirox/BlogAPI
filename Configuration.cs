efee2d7cdd1f63fd5bc579aab91ed67699fd7e20
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-08-23 23:06:17 -0300

    Arquivos de Configuração

---
+++ Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogAPI
+{
+    public static class Configuration
+    {
+        // token - JSON WEB TOKEN
+        public static string JwtKey = "I0p3beps/kijsPm3wiMQnw==";
+
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_kij3nwPmpw/MQ==iss";
+    }
+}
